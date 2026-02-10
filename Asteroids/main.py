78e3bfd4893ba0eca2f607416367098b3eca3733
void-maxxer <apex.ender@example.com>
2026-02-10 18:48:26 +0100

    Shaped up

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
