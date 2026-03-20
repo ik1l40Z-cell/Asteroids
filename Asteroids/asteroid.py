24bb74ea1b6202a37874cf562ccb8d8aa7ef566a
void-maxxer <apex.ender@example.com>
2026-03-20 10:45:38 +0100

    All Lives Gets To Score

--- Asteroids/asteroid.py
+++ Asteroids/asteroid.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from constants import *
+import constants
 from logger import log_event
 from circleshape import CircleShape
 
@@ -9,20 +9,22 @@ class Asteroid(CircleShape):
 		super().__init__(x, y, radius)
 
 	def draw(self, screen):
-		pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+		pygame.draw.circle(screen, "white", self.position, self.radius, constants.LINE_WIDTH)
 
 	def update(self, dt):
 		self.position += self.velocity * dt
 
 	def split(self):
 		self.kill()
-		if self.radius <= ASTEROID_MIN_RADIUS:
+		if self.radius <= constants.ASTEROID_MIN_RADIUS:
+			constants.SCORE += 125
 			return
 		log_event("asteroid_split")
+		constants.SCORE += 42
 		num = random.uniform(20, 50)
 		new_vec1 = self.velocity.rotate(num)
 		new_vec2 = self.velocity.rotate(-num)
-		new_rad = self.radius - ASTEROID_MIN_RADIUS
+		new_rad = self.radius - constants.ASTEROID_MIN_RADIUS
 		smol_ast1 = Asteroid(self.position.x, self.position.y, new_rad)
 		smol_ast2 = Asteroid(self.position.x, self.position.y, new_rad)
 		smol_ast1.velocity = new_vec1
--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from constants import *
+import constants
 from logger import log_state
 from logger import log_event
 from circleshape import CircleShape
@@ -12,9 +12,9 @@ from asteroidfield import AsteroidField
 def main():
     pygame.init()
     print("Starting Asteroids with pygame version: 2.6.1")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     updatable = pygame.sprite.Group()
@@ -26,7 +26,9 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteField = AsteroidField()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    pygame.font.init()
+    font = pygame.font.SysFont(None, 69)
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -37,6 +39,11 @@ def main():
         for drawn in drawable:
             drawn.draw(screen)
         updatable.update(dt)
+        score_surface = font.render(f"Score: {constants.SCORE}", False, "white")
+        screen.blit(score_surface, (10, 10))
+        lives_surface = font.render(f"Lives: {constants.LIVES}", False, "white")
+        x_pos = constants.SCREEN_WIDTH - lives_surface.get_width() - 10
+        screen.blit(lives_surface, (x_pos, 10))
         for asteroid in asteroids:
             for shot in shots:
                 if CircleShape.collides_with(shot, asteroid):
@@ -44,10 +51,15 @@ def main():
                     asteroid.split()
                     shot.kill()
         for asteroid in asteroids:
-            if CircleShape.collides_with(player, asteroid):
-                log_event("player_hit")
+            if CircleShape.collides_with(player, asteroid) and constants.LIVES <= 0:
+                log_event("player_killed")
                 print("Game over!")
                 sys.exit()
+            elif CircleShape.collides_with(player, asteroid):
+                log_event("player_hit")
+                constants.LIVES -= 1
+                player.kill()
+                player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
